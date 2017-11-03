cb256d0d39f6a1b922ce9f32d399819adf1b3889
rrbrink <ryanbrinkerhoff@live.com>
2017-11-02 21:09:20 -0600

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDaZ1LcyBesahUykobyTf8n6V54DueEPsM",
+    authDomain: "movies247-439bd.firebaseapp.com",
+    databaseURL: "https://movies247-439bd.firebaseio.com",
+    projectId: "movies247-439bd",
+    storageBucket: "movies247-439bd.appspot.com",
+    messagingSenderId: "219018323429"
+  };
+  firebase.initializeApp(config);
+var movieImput = $("#movieImput").val();
+
